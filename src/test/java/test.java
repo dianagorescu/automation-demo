664b3a53840f4d68262f5cbd54d362dd7387fc3b
Diana Gorescu <diana_ioana.gorescu@stud.acs.upb.ro>
2024-09-19 17:25:17 +0300

    f

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -6,14 +6,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class test {
     static WebDriver driver;
     @Given("User deschide browser")
     public void deschide_b() {
         System.setProperty("webdriver.edge.driver", "C:/Users/diana/Downloads/edgedriver_win64/msedgedriver.exe");
         driver = new EdgeDriver();
+        driver.manage().window().maximize();
     }
-    @When("User cauta \"emag.ro\"")
+    @When("User navigheaza la google.com")
+    public void navigheaza_la_google() {
+        driver.get("https://www.google.com");
+    }
+    @When("User navigheaza la emag.ro")
     public void deschide_emag() {
         driver.get("https://www.emag.ro");
     }
@@ -23,13 +30,39 @@ public class test {
         searchBoxEmag.sendKeys(produs);
         searchBoxEmag.submit();
     }
+    @And("User primeste lista cu laptopuri Apple")
+    public void verifica_lista_laptopuri_apple() {
+
+        WebElement resultTitle = driver.findElement(By.cssSelector(".card-item .card-v2-title"));
+        assert(resultTitle.getText().toLowerCase().contains("apple"));
+
+        driver.quit();
+    }
     public static void main(String[] args){
         //TO DO
-
-
+        System.setProperty("webdriver.edge.driver", "C:/Users/diana/Downloads/edgedriver_win64/msedgedriver.exe");
+        driver = new EdgeDriver();
 
         driver.manage().window().maximize();
-        driver.findElement(By.id("identifierId")).sendKeys();
+        driver.manage().deleteAllCookies();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("https://www.emag.ro");
+
+        WebElement searchBoxEmag = driver.findElement(By.id("searchboxTrigger"));
+        searchBoxEmag.sendKeys("laptopuri Apple");
+        searchBoxEmag.submit();
+
+        WebElement detail = driver.findElement(By.xpath("//a[@href='https://www.emag.ro/search/laptopuri/filter/tip-procesor-f7885,apple-m2-v31491/laptopuri+Apple/c?ref=lst_leftbar_7885_31491']"));
+        detail.click();
+
+        WebElement resultTitle = driver.findElement(By.cssSelector(".card-item .card-v2-title"));
+        assert(resultTitle.getText().toLowerCase().contains("apple"));
+
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.MINUTES);
+
+        driver.quit();
 
     }
 }
--- src/test/resources/features/cautare.feature
+++ src/test/resources/features/cautare.feature
@@ -1,6 +1,6 @@
 Feature: Cautare laptopuri Apple pe Emag
   Scenario: User nelogat cauta laptopuri
     Given User deschide browser
-    When User cauta "emag.ro"
+    When User navigheaza la emag.ro
     And User cauta "laptopuri Apple"
     Then User primeste lista cu laptopuri Apple
